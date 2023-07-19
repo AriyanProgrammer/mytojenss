e7ac90287f6c26aa5bef6e96f73f7e97a4c74d96
AriyanProgrammer <63957116+AriyanProgrammer@users.noreply.github.com>
2023-07-19 23:24:13 +0330

    Update index.js
    update

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x33296b118406f18aa985b3edc907e8be4c2bda9b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
